8d9081f6df57627bd82af43f6afaa800104721e3
ubuntuVM_DEV <ubuntu@anir0y.vm>
2022-09-15 18:16:38 +0530

    register page  added
    Signed-off-by: ubuntuVM_DEV <ubuntu@anir0y.vm>

--- static/menu.php
+++ static/menu.php
@@ -1,10 +1,13 @@
-<div class="menu">
-  <a href="home.php">HOME</a>
-  <a href="admin.php">admin_panel</a>
-  <a href="upload.php">Upload</a>
-  <a href="search.php">search users</a>
-  <a href="greetings.php">Echo</a>
-  <a href="ping.php">Is Online?</a>
-  <a href="logout.php">Log Out</a>
+<link rel="stylesheet" href="css/menu.css" type="text/css">
+<nav>
+<ul class="wrapper">
+	<li><a href="home.php">Home</a></li>
+	<li><a href="admin.php">admin_panel</a></li>
+	<li><a href="upload.php">Upload</a></li>
+  <li><a href="search.php">search users</a></li>
+	<li><a href="greetings.php">Echo</a></li>
+	<li><a href="ping.php">Is Online?</a></li>
+  <li><a href="logout.php">Log Out</a></li>
 
-</div>
+</ul>
+</nav>
